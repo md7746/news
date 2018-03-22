e4e0526f4f0292be1e09bc7b43fe716e07fbb18c
dm2002 <774681508@qq.com>
2018-03-22 16:15:47 +0800

    存在问题：风‘‘：刚加载出来的时候滚动条会下拉一小段

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,7 +2,8 @@
   <div>
     <section class="search">
       <div>
-        <i class="fa fa-search"></i><input type="text" placeholder="搜索">
+        <i class="fa fa-search"></i>
+        <input type="text" placeholder="搜索">
       </div>
     </section>
     <section>
@@ -14,14 +15,13 @@
           <a @click.prevent="getTabMove(4)" :class="{'item-active':(newItemSelected=='new-item4')}">美女</a>
           <a @click.prevent="getTabMove(5)" :class="{'item-active':(newItemSelected=='new-item5')}">广州</a>
           <a @click.prevent="getTabMove(6)" :class="{'item-active':(newItemSelected=='new-item6')}">体育</a>
-        </nav> 
+        </nav>
         <span class="blue-line" ref='bline'></span>
       </header>
-      
-        <mt-tab-container class="news-list-wrap" v-model="active" swipeable>
-          <mt-tab-container-item id="new-item-con1">
-            <happy-scroll resize>
-              <div class="scroll-wrap" ref="listWrap" :style="{ height: listWrapHeight + 'px' }">
+      <mt-tab-container class="news-list-wrap" v-model="active" swipeable>
+        <mt-tab-container-item id="new-item-con1">
+            <happy-scroll resize scroll-top="130" :style="{ height: listWrapHeight + 'px' }">
+              <div class="scroll-wrap" ref="listWrap">
                 <mt-loadmore :top-method="loadTop" @top-status-change="handleTopChange" :bottom-method="loadBottom" @bottom-status-change="handleBottomChange" :bottom-all-loaded="allLoaded" :topDistance=tdis :bottomDistance=bdis ref="loadmore">
                   <section class="news-list" v-for='newsItem in news'>
                     <h2>{{newsItem.title}}</h2>
@@ -31,17 +31,15 @@
                       </span>
                     </div>
                     <p><span>{{newsItem.source}}</span>
-                    <span>{{newsItem.tcount}}</span>
-                    <span>{{newsItem.ptime}}</span></p>
+                      <span>{{newsItem.tcount}}</span>
+                      <span>{{newsItem.ptime}}</span></p>
                   </section>
-
                   <div slot="top" class="mint-loadmore-top" v-show="tstatu" :class="{'is-top':!this.tstatu}">
                     <span v-show="topStatus !== 'loading'" :class="{ 'is-rotate': topStatus === 'drop' }">↓</span>
                     <span v-show="topStatus === 'loading'">
                       <mt-spinner type="snake"></mt-spinner>
                     </span>
-                  </div>    
-
+                  </div>
                   <div slot="bottom" class="mint-loadmore-bottom">
                     <span v-show="bottomStatus !== 'loading'" :class="{ 'is-rotate': bottomStatus === 'drop' }">↑</span>
                     <span v-show="bottomStatus === 'loading'">
@@ -51,23 +49,23 @@
                 </mt-loadmore>
               </div>
             </happy-scroll>
-          </mt-tab-container-item>
-          <mt-tab-container-item id="new-item-con2">
-            2
-          </mt-tab-container-item>
-          <mt-tab-container-item id="new-item-con3">
-            3
-          </mt-tab-container-item>
-          <mt-tab-container-item id="new-item-con4">
-            4
-          </mt-tab-container-item>
-          <mt-tab-container-item id="new-item-con5">
-            5
-          </mt-tab-container-item>
-          <mt-tab-container-item id="new-item-con6">
-            6
-          </mt-tab-container-item>
-        </mt-tab-container>
+        </mt-tab-container-item>
+        <mt-tab-container-item id="new-item-con2">
+          2
+        </mt-tab-container-item>
+        <mt-tab-container-item id="new-item-con3">
+          3
+        </mt-tab-container-item>
+        <mt-tab-container-item id="new-item-con4">
+          4
+        </mt-tab-container-item>
+        <mt-tab-container-item id="new-item-con5">
+          5
+        </mt-tab-container-item>
+        <mt-tab-container-item id="new-item-con6">
+          6
+        </mt-tab-container-item>
+      </mt-tab-container>
     </section>
     <footer ref="footer">
       <span><i class="fa fa-home"></i><br>首页</span>
@@ -77,231 +75,330 @@
     </footer>
   </div>
 </template>
-
 <script>
-  import {MTween,css} from '@/assets/js/m.Tween.js'
-  import {http} from '@/axios'
-  export default {
-    name: 'Home',
-    data: function() {
-      return {
-        active: 'new-item-con1',
-        newItemSelected: 'new-item1',
-        selected: '外卖',
-        news:[],
-        allLoaded: false,
-        bottomStatus: '',
-        topStatus: '',
-        listWrapHeight:0,
-        tstatu:true,
-        tdis:50,
-        bdis:50,
-        //for the data
-        ti:2,
-        tj:10,
-        bi:5,
-        bj:40
-      }
-    },
-    watch: {
-      "active": { //改变滑动选项样式
-        handler: function(value) {
-          if (value == 'new-item-con1') {
-            this.getMove(0)
-          } else if (value == 'new-item-con2') {
-            this.getMove(1)
-          } else if (value == 'new-item-con3') {
-            this.getMove(2)
-          } else if (value == 'new-item-con4') {
-            this.getMove(3)
-          } else if (value == 'new-item-con5') {
-            this.getMove(4)
-          } else {
-            this.getMove(5)
-          }
+import { MTween, css } from '@/assets/js/m.Tween.js'
+import { http } from '@/axios'
+export default {
+  name: 'Home',
+  data: function() {
+    return {
+      active: 'new-item-con1',
+      newItemSelected: 'new-item1',
+      selected: '外卖',
+      news: [],
+      allLoaded: false,
+      bottomStatus: '',
+      topStatus: '',
+      listWrapHeight: 0,
+      tstatu: true,
+      tdis: 50,
+      bdis: 50,
+      //for the data
+      ti: 2,
+      tj: 10,
+      bi: 5,
+      bj: 40
+    }
+  },
+  watch: {
+    "active": { //改变滑动选项样式
+      handler: function(value) {
+        if (value == 'new-item-con1') {
+          this.getMove(0)
+        } else if (value == 'new-item-con2') {
+          this.getMove(1)
+        } else if (value == 'new-item-con3') {
+          this.getMove(2)
+        } else if (value == 'new-item-con4') {
+          this.getMove(3)
+        } else if (value == 'new-item-con5') {
+          this.getMove(4)
+        } else {
+          this.getMove(5)
         }
       }
-    },
-    created(){
-      this.$nextTick(()=>{//dom渲染完后
+    }
+  },
+  created() {
+    this.$nextTick(() => { //dom渲染完后
 
+    })
+  },
+  mounted() {
+    this.listWrapHeight = this.$refs.footer.getBoundingClientRect().top - this.$refs.listWrap.getBoundingClientRect().top;
+  },
+  methods: {
+    getMove(i) {
+      let that = this;
+      this.tabTween(that, i);
+    },
+    getTabMove(i) {
+      let that = this;
+      this.active = 'new-item-con' + i,
+        this.tabTween(that, i);
+    },
+    tabTween(that, i) {
+      MTween({
+        el: that.$refs.bline,
+        target: {
+          translateX: i * that.$refs.bline.clientWidth
+        },
+        type: 'linear',
+        time: 300,
+        callBack: function() {
+          that.newItemSelected = 'new-item' + (i + 1)
+        }
       })
     },
-    mounted() {
-      this.listWrapHeight = this.$refs.footer.getBoundingClientRect().top - this.$refs.listWrap.getBoundingClientRect().top;
+    handleBottomChange(status) {
+      this.bottomStatus = status;
     },
-    methods: {
-      getMove(i) {
-        let that = this;
-        this.tabTween(that, i);
-      },
-      getTabMove(i) {
-        let that = this;
-        this.active = 'new-item-con' + i,
-          this.tabTween(that, i);
-      },
-      tabTween(that, i) {
+
+    loadBottom() {
+      let that = this;
+      if (that.bi < 9) {
+        that.bi++;
+        that.bj += 10;
+        http.get(that.bi + "-0-10-" + that.bj + ".do").then((response) => {
+          if (response.data.code === 200) {
+            that.news = that.news.concat(response.data.list);
+            that.$refs.loadmore.onBottomLoaded();
+          }
+        }).catch((error) => {
+          console.log(error);
+        });
+      } else {
+        this.allLoaded = true;
+        this.$refs.loadmore.onBottomLoaded();
+      }
+    },
+
+    handleTopChange(status) {
+      this.topStatus = status;
+    },
+    loadTop() {
+      let that = this;
+      if (that.ti < 5) {
+        that.ti++;
+        that.tj += 10;
+        http.get(that.ti + "-0-10-" + that.tj + ".do").then((response) => {
+          if (response.data.code === 200) {
+            that.news = response.data.list.concat(that.news);
+            that.$refs.loadmore.onTopLoaded();
+          }
+        }).catch((error) => {
+          console.log(error);
+        });
+      } else {
+        that.tstatu = false;
         MTween({
-          el: that.$refs.bline,
+          el: that.$refs.listWrap.querySelector(".mint-loadmore-content"),
           target: {
-            translateX: i * that.$refs.bline.clientWidth
+            translateY: 0
           },
           type: 'linear',
-          time: 300,
-          callBack: function() {
-            that.newItemSelected = 'new-item' + (i + 1)
-          }
+          time: 300
         })
-      },
-      handleBottomChange(status) {
-        this.bottomStatus = status;
-      },
-
-      loadBottom() {
-        let that = this;
-        if(that.bi<9){
-          that.bi++;
-          that.bj += 10;
-          http.get(that.bi+"-0-10-"+that.bj+".do").then((response) => {
-            if (response.data.code === 200) {
-              that.news = that.news.concat(response.data.list);
-              that.$refs.loadmore.onBottomLoaded();
-            }
-          }).catch((error) => {
-            console.log(error);
-          });
-        }else{
-          this.allLoaded = true;
-        }
-      },
-
-      handleTopChange(status) {
-        this.topStatus = status;
-      },
-      loadTop() {
-        let that = this;
-        if(that.ti<5){
-          that.ti++;
-          that.tj += 10;
-          http.get(that.ti+"-0-10-"+that.tj+".do").then((response) => {
-            if (response.data.code === 200) {
-              that.news = response.data.list.concat(that.news);
-              that.$refs.loadmore.onTopLoaded();
-            }
-          }).catch((error) => {
-            console.log(error);
-          });
-        }else{
-          that.tstatu=false;
-          MTween({
-            el: that.$refs.listWrap.querySelector(".mint-loadmore-content"),
-            target: {
-              translateY: 0
-            },
-            type: 'linear',
-            time: 300
-          })
-        }
       }
     }
   }
-</script>
+}
 
+</script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='less' scoped>
-  @import '../assets/css/fun.less';
-
-  .search{
-    &{padding: 16/@r 34/@r;z-index: 2;position: relative;background: #fff;}
-    div{height: 28/@r;padding: 17/@r 22/@r;background-color: #f2f2f2;border-radius: 31/@r;-webkit-border-radius: 31/@r;}
-    i{font-size: 28/@r;float: left;color: #8c8c8c;}
-    input{margin-left: 14/@r;font-size: 28/@r;border: none;float: left;background: none;color: #8c8c8c;width: 576/@r;}
+@import '../assets/css/fun.less';
+.search {
+  & {
+    padding: 16/@r 34/@r;
+    z-index: 2;
+    position: relative;
+    background: #fff;
   }
-
-  .new-items-wrap{
-    &{border-bottom: 1px solid #e4e4e4;position: relative;padding: 0 56/@r;z-index: 2;background: #fff;}
-    nav{.flex();}
-    nav a{height: 76/@r;line-height: 76/@r;font-size: 28/@r;text-align: center;.grow();}
-    a.item-active{color: #26a2ff;}
-    .blue-line{position: absolute;display: block;height: 2px;bottom: -1px;background: #26a2ff;width:(750-112)/(6*@r);}
+  div {
+    height: 28/@r;
+    padding: 17/@r 22/@r;
+    background-color: #f2f2f2;
+    border-radius: 31/@r;
+    -webkit-border-radius: 31/@r;
   }
+  i {
+    font-size: 28/@r;
+    float: left;
+    color: #8c8c8c;
+  }
+  input {
+    margin-left: 14/@r;
+    font-size: 28/@r;
+    border: none;
+    float: left;
+    background: none;
+    color: #8c8c8c;
+    width: 576/@r;
+  }
+}
 
-  .news-list-wrap{
-    &{z-index: 1;}
-    .news-list{
-      &{padding-bottom: 46/@r;border-bottom: 1px solid #e4e4e4;}
-      h2{font-size: 32/@r;line-height: 42/@r;color: #3a3a3a;margin-top: 28/@r;}
-      div{.flex();padding: 22/@r 0;}
-      div span{.grow();height: 145/@r;margin-right: 11/@r;display: block;}
-      div span img{height: 100%;width:221/@r;}
-      .no-mr{margin-right: 0;}
-      p{font-size: 24/@r;color: #989898;}
-      p span{margin-right: 24/@r;}
+.new-items-wrap {
+  & {
+    border-bottom: 1px solid #e4e4e4;
+    position: relative;
+    padding: 0 56/@r;
+    z-index: 2;
+    background: #fff;
+  }
+  nav {
+    .flex();
+  }
+  nav a {
+    height: 76/@r;
+    line-height: 76/@r;
+    font-size: 28/@r;
+    text-align: center;
+    .grow();
+  }
+  a.item-active {
+    color: #26a2ff;
+  }
+  .blue-line {
+    position: absolute;
+    display: block;
+    height: 2px;
+    bottom: -1px;
+    background: #26a2ff;
+    width: (750-112)/(6*@r);
+  }
+}
+
+.news-list-wrap {
+  & {
+    z-index: 1;
+    height:100%;
+  }
+  .news-list {
+    & {
+      padding-bottom: 46/@r;
+      border-bottom: 1px solid #e4e4e4;
+    }
+    h2 {
+      font-size: 32/@r;
+      line-height: 42/@r;
+      color: #3a3a3a;
+      margin-top: 28/@r;
+    }
+    div {
+      .flex();
+      padding: 22/@r 0;
+    }
+    div span {
+      .grow();
+      height: 145/@r;
+      margin-right: 11/@r;
+      display: block;
+    }
+    div span img {
+      height: 100%;
+      width: 221/@r;
+    }
+    .no-mr {
+      margin-right: 0;
+    }
+    p {
+      font-size: 24/@r;
+      color: #989898;
+    }
+    p span {
+      margin-right: 24/@r;
     }
   }
+}
 
-  .scroll-wrap{height:100%;padding: 0 34/@r;}
+.scroll-wrap {
+  height: 100%;
+  padding: 0 34/@r;
+}
 
-  footer{
-    &{z-index: 2;background: #fff;position: absolute;left:0;bottom: 0;right: 0;.flex();padding: 14/@r 0;border-top: 1px solid #e4e4e4;}
-    span{.grow();text-align: center;font-size: 20/@r;}
-    span i{font-size: 40/@r;margin-bottom: 12/@r;}
+footer {
+  & {
+    z-index: 2;
+    background: #fff;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    .flex();
+    padding: 14/@r 0;
+    border-top: 1px solid #e4e4e4;
   }
-  //上下拉加载
-  .loading-background, .mint-loadmore-top span {
-      -webkit-transition: .2s linear;
-      transition: .2s linear
+  span {
+    .grow();
+    text-align: center;
+    font-size: 20/@r;
   }
-  .mint-loadmore-top span {
-      display: inline-block;
-      vertical-align: middle
+  span i {
+    font-size: 40/@r;
+    margin-bottom: 12/@r;
   }
+} 
 
-  .mint-loadmore-top span.is-rotate {
-      -webkit-transform: rotate(180deg);
-      transform: rotate(180deg)
-  }
+//上下拉加载
+.loading-background,
+.mint-loadmore-top span {
+  -webkit-transition: .2s linear;
+  transition: .2s linear
+}
 
-  .page-loadmore .mint-spinner {
-      display: inline-block;
-      vertical-align: middle
-  }
+.mint-loadmore-top span {
+  display: inline-block;
+  vertical-align: middle
+}
 
-  .page-loadmore-desc {
-      text-align: center;
-      color: #666;
-      padding-bottom: 5px
-  }
+.mint-loadmore-top span.is-rotate {
+  -webkit-transform: rotate(180deg);
+  transform: rotate(180deg)
+}
 
-  .page-loadmore-desc:last-of-type,
-  .page-loadmore-listitem {
-      border-bottom: 1px solid #eee
-  }
+.page-loadmore .mint-spinner {
+  display: inline-block;
+  vertical-align: middle
+}
 
-  .page-loadmore-listitem {
-      height: 50px;
-      line-height: 50px;
-      text-align: center
-  }
+.page-loadmore-desc {
+  text-align: center;
+  color: #666;
+  padding-bottom: 5px
+}
 
-  .page-loadmore-listitem:first-child {
-      border-top: 1px solid #eee
-  }
+.page-loadmore-desc:last-of-type,
+.page-loadmore-listitem {
+  border-bottom: 1px solid #eee
+}
 
-  .page-loadmore-wrapper {
-      
-  }
+.page-loadmore-listitem {
+  height: 50px;
+  line-height: 50px;
+  text-align: center
+}
 
-  .mint-loadmore-bottom span {
-      display: inline-block;
-      -webkit-transition: .2s linear;
-      transition: .2s linear;
-      vertical-align: middle
-  }
+.page-loadmore-listitem:first-child {
+  border-top: 1px solid #eee
+}
 
-  .mint-loadmore-bottom span.is-rotate {
-      -webkit-transform: rotate(180deg);
-      transform: rotate(180deg)
-  }
-  .is-top{transform: translate3d(0px, 0px, 0px);-webkit-transform: translate3d(0px, 0px, 0px);}
+.page-loadmore-wrapper {}
+
+.mint-loadmore-bottom span {
+  display: inline-block;
+  -webkit-transition: .2s linear;
+  transition: .2s linear;
+  vertical-align: middle
+}
+
+.mint-loadmore-bottom span.is-rotate {
+  -webkit-transform: rotate(180deg);
+  transform: rotate(180deg)
+}
+
+.is-top {
+  transform: translate3d(0px, 0px, 0px);
+  -webkit-transform: translate3d(0px, 0px, 0px);
+}
 </style>
--- src/components/test.vue
+++ src/components/test.vue
@@ -1,156 +1,149 @@
 <template>
   <div class="page-loadmore">
-
-<happy-scroll resize>
-    <div class="page-loadmore-wrapper" ref="wrapper">
-      <mt-loadmore :top-method="loadTop" @top-status-change="handleTopChange"  :bottom-method="loadBottom" @bottom-status-change="handleBottomChange" :bottom-all-loaded="allLoaded" ref="loadmore">
-        
-        <ul class="page-loadmore-list">
-          <li v-for="item in list" class="page-loadmore-listitem">{{ item }}</li>
-        </ul>
-
-
-       <div slot="top" class="mint-loadmore-top">
-          <span v-show="topStatus !== 'loading'" :class="{ 'is-rotate': topStatus === 'drop' }">↓</span>
-          <span v-show="topStatus === 'loading'">
+    <p>-------------------</p>
+    <div style="height:300px">
+      <happy-scroll resize>
+        <mt-loadmore :top-method="loadTop" @top-status-change="handleTopChange" :bottom-method="loadBottom" @bottom-status-change="handleBottomChange" :bottom-all-loaded="allLoaded" ref="loadmore">
+          <ul class="page-loadmore-list">
+            <li v-for="item in list" class="page-loadmore-listitem">{{ item }}</li>
+          </ul>
+          <div slot="top" class="mint-loadmore-top">
+            <span v-show="topStatus !== 'loading'" :class="{ 'is-rotate': topStatus === 'drop' }">↓</span>
+            <span v-show="topStatus === 'loading'">
             <mt-spinner type="snake"></mt-spinner>
           </span>
-        </div>    
-
-
-        <div slot="bottom" class="mint-loadmore-bottom">
-          <span v-show="bottomStatus !== 'loading'" :class="{ 'is-rotate': bottomStatus === 'drop' }">↑</span>
-          <span v-show="bottomStatus === 'loading'">
+          </div>
+          <div slot="bottom" class="mint-loadmore-bottom">
+            <span v-show="bottomStatus !== 'loading'" :class="{ 'is-rotate': bottomStatus === 'drop' }">↑</span>
+            <span v-show="bottomStatus === 'loading'">
             <mt-spinner type="snake"></mt-spinner>
           </span>
-        </div>
-      </mt-loadmore>
-
-    </div></happy-scroll>
+          </div>
+        </mt-loadmore>
+      </happy-scroll>
+    </div>
+    <p>-------------------</p>
   </div>
 </template>
-
 <style>
- 
+.loading-background,
+.mint-loadmore-top span {
+  -webkit-transition: .2s linear;
+  transition: .2s linear
+}
 
+.mint-loadmore-top span {
+  display: inline-block;
+  vertical-align: middle
+}
 
-	.loading-background, .mint-loadmore-top span {
-	    -webkit-transition: .2s linear;
-	    transition: .2s linear
-	}
-	.mint-loadmore-top span {
-	    display: inline-block;
-	    vertical-align: middle
-	}
+.mint-loadmore-top span.is-rotate {
+  -webkit-transform: rotate(180deg);
+  transform: rotate(180deg)
+}
 
-	.mint-loadmore-top span.is-rotate {
-	    -webkit-transform: rotate(180deg);
-	    transform: rotate(180deg)
-	}
+.page-loadmore .mint-spinner {
+  display: inline-block;
+  vertical-align: middle
+}
 
-	.page-loadmore .mint-spinner {
-	    display: inline-block;
-	    vertical-align: middle
-	}
+.page-loadmore-desc {
+  text-align: center;
+  color: #666;
+  padding-bottom: 5px
+}
 
-	.page-loadmore-desc {
-	    text-align: center;
-	    color: #666;
-	    padding-bottom: 5px
-	}
+.page-loadmore-desc:last-of-type,
+.page-loadmore-listitem {
+  border-bottom: 1px solid #eee
+}
 
-	.page-loadmore-desc:last-of-type,
-	.page-loadmore-listitem {
-	    border-bottom: 1px solid #eee
-	}
+.page-loadmore-listitem {
+  height: 50px;
+  line-height: 50px;
+  text-align: center
+}
 
-	.page-loadmore-listitem {
-	    height: 50px;
-	    line-height: 50px;
-	    text-align: center
-	}
+.page-loadmore-listitem:first-child {
+  border-top: 1px solid #eee
+}
 
-	.page-loadmore-listitem:first-child {
-	    border-top: 1px solid #eee
-	}
+.page-loadmore-wrapper {}
 
-	.page-loadmore-wrapper {
+.mint-loadmore-bottom span {
+  display: inline-block;
+  -webkit-transition: .2s linear;
+  transition: .2s linear;
+  vertical-align: middle
+}
 
-	}
-
-	.mint-loadmore-bottom span {
-	    display: inline-block;
-	    -webkit-transition: .2s linear;
-	    transition: .2s linear;
-	    vertical-align: middle
-	}
-
-	.mint-loadmore-bottom span.is-rotate {
-	    -webkit-transform: rotate(180deg);
-	    transform: rotate(180deg)
-	}
+.mint-loadmore-bottom span.is-rotate {
+  -webkit-transform: rotate(180deg);
+  transform: rotate(180deg)
+}
 
 </style>
-
 <script type="text/babel">
- import {MTween} from '@/assets/js/m.Tween.js'
-  export default {
-    data() {
-      return {
-        list: [],
-        allLoaded: false,
+import { MTween } from '@/assets/js/m.Tween.js'
+export default {
+  data() {
+    return {
 
-        bottomStatus: '',
-        wrapperHeight: 0,
+      list: [],
+      allLoaded: false,
 
-        topStatus: ''
-      };
+      bottomStatus: '',
+      wrapperHeight: 0,
+
+      topStatus: ''
+    };
+  },
+
+  methods: {
+    handleBottomChange(status) {
+      console.log(status)
+      this.bottomStatus = status;
     },
 
-    methods: {
-      handleBottomChange(status) {
-        console.log(status)
-        this.bottomStatus = status;
-      },
-
-      loadBottom() {
-        setTimeout(() => {
-          let lastValue = this.list[this.list.length - 1];
-          if (lastValue < 40) {
-            for (let i = 1; i <= 10; i++) {
-              this.list.push(lastValue + i);
-            }
-          } else {
-            this.allLoaded = true;
-          }
-          this.$refs.loadmore.onBottomLoaded();
-        }, 1500);
-      },
-
-      handleTopChange(status) {
-
-        this.topStatus = status;
-      },
-      loadTop() {
-        setTimeout(() => {
-          let firstValue = this.list[0];
+    loadBottom() {
+      setTimeout(() => {
+        let lastValue = this.list[this.list.length - 1];
+        if (lastValue < 20) {
           for (let i = 1; i <= 3; i++) {
-            this.list.unshift(firstValue - i);
+            this.list.push(lastValue + i);
           }
-          this.$refs.loadmore.onTopLoaded();
-        }, 1500);
-      },
-    
-},
-
-    created() {
-      for (let i = 1; i <= 8; i++) {
-        this.list.push(i);
-      }
+        } else {
+          this.allLoaded = true;
+        }
+        this.$refs.loadmore.onBottomLoaded();
+      }, 1500);
     },
 
-    mounted() {
-      this.wrapperHeight = document.documentElement.clientHeight - this.$refs.wrapper.getBoundingClientRect().top;
+    handleTopChange(status) {
+
+      this.topStatus = status;
+    },
+    loadTop() {
+      setTimeout(() => {
+        let firstValue = this.list[0];
+        for (let i = 1; i <= 3; i++) {
+          this.list.unshift(firstValue - i);
+        }
+        this.$refs.loadmore.onTopLoaded();
+      }, 1500);
+    },
+
+  },
+
+  created() {
+    for (let i = 1; i <= 8; i++) {
+      this.list.push(i);
     }
-  };
+  },
+
+  mounted() {
+
+  }
+};
+
 </script>
